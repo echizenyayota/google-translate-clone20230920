71eba766ee48a763ff838c25d410062bbe7b9e88
echizenyayota <virtualmisso30@gmail.com>
2023-09-26 21:50:29 +0900

    46:50 get the names of the languages by using Objet.keys

--- src/App.js
+++ src/App.js
@@ -26,7 +26,8 @@ const App = () => {
     try {
       const response = await axios.request(options);
       console.log(response.data);
-      setLanguages(response.data);
+      const arrayOfData = Object.keys(response.data.data).map(key => response.data.data[key])
+      setLanguages(arrayOfData);
     } catch (error) {
       console.error(error);
     }
