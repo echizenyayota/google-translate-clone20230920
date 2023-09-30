18d2fe42cfe0c7c68532ed05d6f21cf43f5e5e2e
echizenyayota <virtualmisso30@gmail.com>
2023-09-30 15:31:48 +0900

    1:23:33 set the routing to call for the languages to translate

--- server.js
+++ server.js
@@ -5,7 +5,26 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-
 app.use(cors());
+
+app.get("/languages", async(req, res) => {
+  const options = {
+    method: "GET",
+    headers: {
+      'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_HOST,
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    },
+
+  }
+
+  try {
+    const response = await axios("https://g-translate1.p.rapidapi.com/languages", options);
+    const arrayOfData = Object.keys(response.data.data).map(key => response.data.data[key]);
+    res.status(200).json(arrayOfData);
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({message: err});
+  }
+});
 
 app.listen(PORT, () => console.log("Server running on PORT" + PORT));
